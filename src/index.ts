d5d2c4f2bf23de9d96edfdf35c525c6f126918b4
Federico "Edo" Granata <3602209+Edo78@users.noreply.github.com>
2023-12-17 09:44:18 +0100

    Initial commit for the template

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// this is the entry point of the application
+// just for the template
+console.log("Hello, world!");
---
+++ tests/fake.test.ts
@@ -0,0 +1,4 @@
+// this is a fake test to make sure that the test suite is working
+test('checks if 1 is 1', () => {
+    expect(1).toBe(1);
+});
